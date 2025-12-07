d8f33314ec1ee1ec30fedadef1881297ddb93e53
LusmicSam <138333123+LusmicSam@users.noreply.github.com>
2025-12-07 18:23:24 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/UNIT4L1',
+  assetPrefix: '/UNIT4L1',
 };
 
 export default nextConfig;
